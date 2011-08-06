836bc9f80aa94b025ae3bb75633704dbd88f9334
John Hinnegan <john.hinnegan@gmail.com>
2011-08-06 14:04:58 -0700

    initial rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MultistepSignup::Application.config.secret_token = '52e395126e87bc982c1f3621a0a869ff7b3cd8d35310a076ee9e86ceeb3cfa3d7b41f6edea8e97c096cb9a6d07ae76e1ac8734bbd88e985f7e83d0a60df15aac'
