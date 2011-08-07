88572388bbcdc40141e7f1d3082e74c97a451be3
John Hinnegan <john.hinnegan@gmail.com>
2011-08-06 20:12:14 -0700

    redirect on sign in properly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource) 
     if resource.is_a?(User)
-      return user_path(resource)
+      return user_show
     else 
       super
     end 
